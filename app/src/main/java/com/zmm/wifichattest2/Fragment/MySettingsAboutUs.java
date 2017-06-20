9110a8b711ad4450c9ad3dc566e4dfc79cd68de1
Giousa <65489469@qq.com>
2017-06-20 14:01:56 +0800

    first commit

---
+++ app/src/main/java/com/zmm/wifichattest2/Fragment/MySettingsAboutUs.java
@@ -0,0 +1,16 @@
+package com.zmm.wifichattest2.Fragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zmm.wifichattest2.R;
+
+
+public class MySettingsAboutUs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_settings_about_us);
+    }
+}
---
+++ app/src/main/java/com/zmm/wifichattest2/Fragment/MySettingsHelp.java
@@ -0,0 +1,15 @@
+package com.zmm.wifichattest2.Fragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zmm.wifichattest2.R;
+
+public class MySettingsHelp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_settings_help);
+    }
+}
